4fb2371ff1fd07d0c7dea1141981d5694d482207
adk-nih <85114620+adk-nih@users.noreply.github.com>
2022-05-15 00:41:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['6288220195739'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['6288220195739', 'DS', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'Ara Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
